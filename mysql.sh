40a884ec8d7c6c2ee083f11221a7a8c0d32c5183
Naresh Gantala <nareshgantala@gmail.com>
2026-04-21 11:20:40 +0530

    up

---
+++ mysql.sh
@@ -0,0 +1,14 @@
+dnf install -y mysql8.4-server
+
+
+
+mysql -u root -e "
+  CREATE USER 'root'@'%' IDENTIFIED BY 'RoboShop@1';
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+  ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
+  FLUSH PRIVILEGES;
+"
+
+systemctl enable mysqld
+systemctl start mysqld
+
